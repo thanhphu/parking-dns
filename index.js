a24461d40700eb8f735a0c5605502282b6ae2462
Thanh Phu <git@thanhphu.net>
2020-03-11 12:36:56 +0900

    Working, add vscode debug configuration

--- index.js
+++ index.js
@@ -4,19 +4,29 @@ let ttl = 300;
 let port = 9999;
 
 server.listen(port, '0.0.0.0', function () {
-  console.log('DNS server started on port ', port);
+  console.log('DNS server started on port', port);
 });
 
 server.on('query', function (query) {
   var domain = query.name();
   console.log('DNS Query: %s', domain)
-  query.addAnswer(domain, new named.SOARecord(domain, {
-    serial: 201905150000
-  }), ttl);
-  query.addAnswer(domain, new named.ARecord("183.111.125.105"), ttl);
-  query.addAnswer(domain, new named.MXRecord("mx1.forwardemail.net", {
-    priority: 10
-  }), ttl);
-  query.addAnswer(domain, new named.TXTRecord("forward-email=inquiry-" + domain + "@netviet.kr"), ttl);
+  switch (query.type()) {
+    case "SOA":
+      query.addAnswer(domain, new named.SOARecord(domain, {
+        serial: 201905150000
+      }), ttl);
+      break;
+    case "A":
+      query.addAnswer(domain, new named.ARecord("183.111.125.105"), ttl);
+      break;
+    case "MX":
+      query.addAnswer(domain, new named.MXRecord("mx1.forwardemail.net", {
+        priority: 10
+      }), ttl);
+      break;
+    case "TXT":
+      query.addAnswer(domain, new named.TXTRecord("forward-email=inquiry-" + domain + "@netviet.kr"), ttl);
+      break;
+  }
   server.send(query);
 });
